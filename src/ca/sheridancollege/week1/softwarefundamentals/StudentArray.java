f17eba37db766ae6cd343140ab9a4301133360da
Cory Salmon <salmocor@sheridancollege.ca>
2019-01-24 13:15:57 -0500

    This is another revert, but this revert is of a revert

--- src/ca/sheridancollege/week1/softwarefundamentals/StudentArray.java
+++ src/ca/sheridancollege/week1/softwarefundamentals/StudentArray.java
@@ -14,13 +14,12 @@ public class StudentArray
     public static void main(String[] args)
     {
         System.out.println("This is a new message");
-        StudentDemo [] studentList = new StudentDemo[5];
+        StudentDemo [] studentList = new StudentDemo[4];
         StudentDemo student1 = new StudentDemo("1", "Jim Halpern");
         studentList[0] = student1;
         studentList[1] = new StudentDemo("2", "Michael Scott");
         studentList[2] = new StudentDemo("3", "Pam Beasley");
         studentList[3] = new StudentDemo("4", "Dwight Schrute");
-        studentList[4] = new StudentDemo("5", "Toby Flendersen");
         
         for(int i=0; i<studentList.length; i++)
         {
